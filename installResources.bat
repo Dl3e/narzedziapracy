81e35bf5c0ef0c47406eef2661b641f2f9295989
Dl3e <93380481+Dl3e@users.noreply.github.com>
2024-05-22 13:08:58 +0200

    Update installResources.bat

--- installResources.bat
+++ installResources.bat
@@ -1,4 +1,4 @@
-\Scripts\pip.exe install simplejson==3.19.2
-\Scripts\pip.exe install PyYAML
-\Scripts\pip.exe install xmltodict
-\Scripts\pip.exe install pyinstaller
+pip.exe install simplejson==3.19.2
+pip.exe install PyYAML
+pip.exe install xmltodict
+pip.exe install pyinstaller
